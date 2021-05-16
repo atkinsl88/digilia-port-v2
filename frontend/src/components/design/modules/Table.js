c2a02b437cb6a30ef030f501f2bc6e637d78dac6
atkinsl88 <liamatkins24@gmail.com>
2021-05-16 22:34:05 +0100

    updates

--- frontend/src/components/design/modules/Table.js
+++ frontend/src/components/design/modules/Table.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Table = () => (
   <section>
@@ -21,13 +20,74 @@ const Table = () => (
       </div>
       <div className="table-row">
         <div className="table-item-xlng">
-          <p>Park House Care - Redesign</p>
+          <p>Portfolio v2</p>
         </div>
         <div className="table-item-xlng mob-hidden">
           <p>Responsive website design.</p>
         </div>
         <div className="table-item-xlng">
-          <Link to="https://invis.io/ZHYOH7XAJKU" target="_blank">Invision Board</Link>
+          <a href="https://sketch.cloud/s/46a11022-adda-4dc3-8829-3a6cd7edbd43" target="_blank" rel="noreferrer">Sketch Board</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="table-year">
+      <h2>2020</h2>
+    </div>
+    <div className="table-container">
+      <div className="table-headings">
+        <div className="table-item-xlng">
+          <p><b>Project Name</b></p>
+        </div>
+        <div className="table-item-xlng mob-hidden">
+          <p><b>Description</b></p>
+        </div>
+        <div className="table-item-xlng">
+          <p><b>Link</b></p>
+        </div>
+      </div>
+      <div className="table-row">
+        <div className="table-item-xlng">
+          <p>Portfolio v1</p>
+        </div>
+        <div className="table-item-xlng mob-hidden">
+          <p>Responsive website design.</p>
+        </div>
+        <div className="table-item-xlng">
+          <a href="https://sketch.cloud/s/3f986dcc-707f-4fe0-8bc1-3447d7f89094" target="_blank" rel="noreferrer">Sketch Board</a>
+        </div>
+      </div>
+      <div className="table-row">
+        <div className="table-item-xlng">
+          <p>Park House Care</p>
+        </div>
+        <div className="table-item-xlng mob-hidden">
+          <p>Responsive website design.</p>
+        </div>
+        <div className="table-item-xlng">
+          <a href="https://sketch.cloud/s/d5fbc583-7066-4051-a887-924a3443fe35" target="_blank" rel="noreferrer">Sketch Board</a>
+        </div>
+      </div>
+      <div className="table-row">
+        <div className="table-item-xlng">
+          <p>Pensacola</p>
+        </div>
+        <div className="table-item-xlng mob-hidden">
+          <p>Responsive website design.</p>
+        </div>
+        <div className="table-item-xlng">
+          <a href="https://sketch.cloud/s/089878b1-1c00-470c-9563-148742c355ca" target="_blank" rel="noreferrer">Sketch Board</a>
+        </div>
+      </div>
+      <div className="table-row">
+        <div className="table-item-xlng">
+          <p>Zeldas Quest</p>
+        </div>
+        <div className="table-item-xlng mob-hidden">
+          <p>Interactive game design.</p>
+        </div>
+        <div className="table-item-xlng">
+          <a href="https://sketch.cloud/s/86834841-e86c-483c-af4d-3d48eeec7215" target="_blank" rel="noreferrer">Sketch Board</a>
         </div>
       </div>
     </div>
